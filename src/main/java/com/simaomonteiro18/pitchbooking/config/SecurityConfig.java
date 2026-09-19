871ba33dbe9ddf007b14e7767065f5e1f0abf86e
simaomonteiro18 <sasmonteiro07@gmail.com>
2026-09-19 19:53:41 +0100

    feat: add JwtFilter to authenticate requests via Bearer token

--- src/main/java/com/simaomonteiro18/pitchbooking/config/SecurityConfig.java
+++ src/main/java/com/simaomonteiro18/pitchbooking/config/SecurityConfig.java
@@ -1,14 +1,19 @@
 package com.simaomonteiro18.pitchbooking.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 public class SecurityConfig {
+
+    @Autowired
+    private JwtFilter jwtFilter;
 
     @Bean
     public PasswordEncoder passwordEncoderGenerator() {
@@ -23,7 +28,8 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth.requestMatchers("/pitches", "/users", "/login").permitAll().anyRequest().authenticated());
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/pitches", "/users", "/login").permitAll().anyRequest().authenticated())
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
 
